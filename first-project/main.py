6fb7350e37608dd86a7d8403bb5e41ae0c705d16
kabigon23 <kabigond23@gmail.com>
2023-07-02 01:27:10 +0900

    Beta version 0.1

--- first-project/main.py
+++ first-project/main.py
@@ -20,9 +20,11 @@ async def main():
     WHITE = (255, 255, 255)
     YELLOW = (245, 245, 66)
 
+
+
     # 게임 변수
-    WIDTH = 400
-    HEIGHT = 600
+    WIDTH = 360
+    HEIGHT = 800
     SPEED = 1
     MONEY = 10000
     LEVEL = 2000
@@ -30,10 +32,20 @@ async def main():
     PM = BLACK
     PLUS = ""
     waiting = True
+    mouse_pressed_left = False
+    mouse_pressed_right = False
     size_list = ["short", "medium", "long", "llong"]
 
+    # L과 R의 가로 크기와 구분선의 두께 정의
+    l_width = 180
+    divider_thickness = 2
+
+    # 구분선의 좌표와 크기 계산
+    divider_y = 600
+    divider_height = HEIGHT - divider_y
+
     # 폰트 설정
-    font = pygame.font.SysFont("Verdana", 60)
+    font = pygame.font.SysFont("Verdana", 40)
     font_small = pygame.font.SysFont("Verdana", 20)
 
     start_text = font_small.render("Press Enter to Start", True, WHITE)
@@ -47,6 +59,9 @@ async def main():
     effect_text_m10 = font_small.render("-10%", True, BLUE)
     effect_text_m25 = font_small.render("-25%", True, BLUE)
     effect_text_m50 = font_small.render("-50%", True, BLUE)
+    l_text = font.render("L", True, YELLOW)
+    r_text = font.render("R", True, YELLOW)
+
 
 
     # 픽셀아트 만들기
@@ -135,7 +150,7 @@ async def main():
                 self.size = rand_size
                 self.image = pygame.image.load(f"red_stick_{rand_size}.png")
                 self.rect.top = 0
-                self.rect.center = (random.randint(10, 390), 0)
+                self.rect.center = (random.randint(40, WIDTH-40), 0)
 
         def draw(self, surface):
             surface.blit(self.image, self.rect)
@@ -155,7 +170,7 @@ async def main():
                 self.size = rand_size
                 self.image = pygame.image.load(f"blue_stick_{rand_size}.png")
                 self.rect.top = 0
-                self.rect.center = (random.randint(10, 390), 0)
+                self.rect.center = (random.randint(40, WIDTH-40), 0)
 
         def draw(self, surface):
             surface.blit(self.image, self.rect)
@@ -164,11 +179,17 @@ async def main():
     class Player(pygame.sprite.Sprite):
         def __init__(self):
             super().__init__()
-            self.image = pygame.image.load("player1.png")
+            self.image = pygame.image.load("normal.png")
             self.rect = self.image.get_rect()
-            self.rect.center = (160,520)
+            self.rect.center = (WIDTH / 2,550)
 
         def move(self):
+            if rates < -50:
+                self.image = pygame.image.load("sad_face.png")
+            elif rates > 200:
+                self.image = pygame.image.load("smile_face.png")
+            else:
+                self.image = pygame.image.load("normal_face.png")    
             pressed_keys = pygame.key.get_pressed()
             if self.rect.left > 0:
                 if pressed_keys[K_LEFT]:
@@ -199,7 +220,7 @@ async def main():
 
     # 시작화면 생성
     DISPLAYSURF.fill(BLACK)
-    DISPLAYSURF.blit(start_text, (100, 300))
+    DISPLAYSURF.blit(start_text, ((WIDTH - start_text.get_width()) // 2 , (HEIGHT - start_text.get_height()) // 2))
 
     # 게임 루프 시작
     while True:
@@ -212,13 +233,37 @@ async def main():
                 elif event.type == KEYDOWN:
                     if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
                         waiting = False
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        waiting = False
+
+
 
         for event in pygame.event.get():
             if event.type == INC_SPEED:
-                SPEED += 1
+                if SPEED < 40:
+                    SPEED += 1.5
             if event.type == pygame.QUIT: # 종료 조건
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if event.pos:
+                        x,y = event.pos
+                        if x <= WIDTH / 2 and y > 600:
+                            mouse_pressed_left = True
+                        elif x > WIDTH / 2 and y > 600:
+                            mouse_pressed_right = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    mouse_pressed_left = False
+                    mouse_pressed_right = False
+
+        if P1.rect.left > 0 and mouse_pressed_left:
+            P1.rect.move_ip(-5,0)
+        if P1.rect.right < WIDTH and mouse_pressed_right:
+            P1.rect.move_ip(5,0)            
+
         if rates > 0:
             PM = RED
             PLUS = "+"
@@ -262,20 +307,49 @@ async def main():
 
             rates = MONEY / 100 - 100
             collided_yangbong.kill()
+            Y1 = Yangbong(size_list[random.randint(0,3)])
+            all_sprites.add(Y1)
+            yangbongs.add(Y1)
             if rates >= 1000:
                         
                 DISPLAYSURF.fill(RED)
-                DISPLAYSURF.blit(game_success, (15,250))
+                DISPLAYSURF.blit(game_success, ((WIDTH - game_over_text.get_width()) // 2 , (HEIGHT - game_over_text.get_height()) // 2))
                 
                 pygame.display.update()
                 for entity in all_sprites:
                         entity.kill() 
                 time.sleep(2)
-                pygame.quit()
-                sys.exit()             
-            Y1 = Yangbong(size_list[random.randint(0,3)])
-            all_sprites.add(Y1)
-            yangbongs.add(Y1)
+                waiting = True
+                # pygame.quit()
+                # sys.exit()
+                while waiting:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        elif event.type == MOUSEBUTTONDOWN:
+                            if event.button == 1:
+                                waiting = False
+                                SPEED = 1
+                                MONEY = 10000
+                                rates = 0             
+                                all_sprites.add(P1)
+                                all_sprites.add(Y1)
+                                all_sprites.add(E1)
+                                yangbongs.add(Y1)
+                                embongs.add(E1)
+                        elif event.type == KEYDOWN:
+                            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+                                waiting = False
+                                SPEED = 1
+                                MONEY = 10000
+                                rates = 0             
+                                all_sprites.add(P1)
+                                all_sprites.add(Y1)
+                                all_sprites.add(E1)
+                                yangbongs.add(Y1)
+                                embongs.add(E1)           
+
 
         collided_embong = pygame.sprite.spritecollideany(P1, embongs)
         if collided_embong:
@@ -299,20 +373,61 @@ async def main():
             rates = MONEY / 100 - 100
             
             collided_embong.kill()
+            E1 = Embong(size_list[random.randint(0,3)])
+            all_sprites.add(E1)
+            embongs.add(E1)
             if rates <= -95:                     
                 DISPLAYSURF.fill(RED)
-                DISPLAYSURF.blit(game_over_text, (30,250))
+                DISPLAYSURF.blit(game_over_text, ((WIDTH - game_over_text.get_width()) // 2 , (HEIGHT - game_over_text.get_height()) // 2))
                 
                 pygame.display.update()
                 for entity in all_sprites:
                         entity.kill() 
                 time.sleep(2)
-                pygame.quit()
-                sys.exit()             
-            E1 = Embong(size_list[random.randint(0,3)])
-            all_sprites.add(E1)
-            embongs.add(E1)
+                waiting = True
+                # pygame.quit()
+                # sys.exit()
+                while waiting:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        elif event.type == MOUSEBUTTONDOWN:
+                            if event.button == 1:
+                                waiting = False
+                                SPEED = 1
+                                MONEY = 10000
+                                rates = 0             
+                                all_sprites.add(P1)
+                                all_sprites.add(Y1)
+                                all_sprites.add(E1)
+                                yangbongs.add(Y1)
+                                embongs.add(E1)
 
+                        elif event.type == KEYDOWN:
+                            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+                                waiting = False
+                                SPEED = 1
+                                MONEY = 10000
+                                rates = 0             
+                                all_sprites.add(P1)
+                                all_sprites.add(Y1)
+                                all_sprites.add(E1)
+                                yangbongs.add(Y1)
+                                embongs.add(E1)
+        # 왼쪽 구역 그리기 (검은색 배경에 흰색 L)
+        pygame.draw.rect(DISPLAYSURF, BLACK, (0, divider_y, l_width, divider_height))
+        pygame.draw.rect(DISPLAYSURF, WHITE, (0, divider_y, l_width, divider_thickness))
+        l_text_rect = l_text.get_rect(center=(l_width // 2, divider_y + divider_height // 2))
+        DISPLAYSURF.blit(l_text, l_text_rect)
+
+        # 오른쪽 구역 그리기 (검은색 배경에 흰색 R)
+        r_x = l_width + divider_thickness
+        r_width = WIDTH - r_x
+        pygame.draw.rect(DISPLAYSURF, BLACK, (r_x, divider_y, r_width, divider_height))
+        pygame.draw.rect(DISPLAYSURF, WHITE, (r_x, divider_y, r_width, divider_thickness))
+        r_text_rect = r_text.get_rect(center=(r_x + r_width // 2, divider_y + divider_height // 2))
+        DISPLAYSURF.blit(r_text, r_text_rect)        
 
         pygame.display.update() # 이 함수가 호출되기 전까진 화면 변화가 일어나지 않는다.
         FPS.tick(FPS_SEC)
